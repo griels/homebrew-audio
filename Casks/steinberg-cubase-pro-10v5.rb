3a332cb260c808a8409d27db447736c5baa189ca
Ellis Breen <ellis.breen@gmail.com>
2020-05-17 16:21:48 +0100

    Add Cubase Pro 10.5

---
+++ Casks/steinberg-cubase-pro-10v5.rb
@@ -0,0 +1,59 @@
+
+cask 'steinberg-cubase-pro-10v5' do
+  version '10.5.0'
+  sha256 'b7a6fe2cb0ba266bb0827977298046ec8f1bf64017299c310ddaa7bf96496e4c'
+
+  url "https://download.steinberg.net/downloads_software/Cubase_10.5/Cubase_10.5_Installer_mac.dmg"
+  name 'Steinberg Cubase Pro 10.5'
+  homepage 'https://www.steinberg.net/en/products/cubase'
+
+  pkg 'Cubase for Mac/Cubase 10.5.pkg'
+
+  uninstall pkgutil: [
+                       'com.steinberg.*.cubase95',
+                       'com.steinberg.AcousticAgentSEMixer',
+                       'com.steinberg.AcousticAgentSEPatternBanks',
+                       'com.steinberg.AcousticAgentSEStyleFiles',
+                       'com.steinberg.Cubase10v5',
+                       'com.steinberg.Documentation.hsse3',
+                       'com.steinberg.Documentation.retrologue',
+                       'com.steinberg.GrooveAgentFamily-SubPresetsShared',
+                       'com.steinberg.GrooveAgentSE-SubPresets',
+                       'com.steinberg.GrooveAgentSE',
+                       'com.steinberg.HALionFamily-SubPresetsShared',
+                       'com.steinberg.HALionIcon',
+                       'com.steinberg.HALionLibraryManager',
+                       'com.steinberg.HALionSonicSE-SubPresets',
+                       'com.steinberg.HALionSonicSE.hsse3',
+                       'com.steinberg.Padshop',
+                       'com.steinberg.PadshopProVST3Presets',
+                       'com.steinberg.PadshopSubPresets',
+                       'com.steinberg.PadshopVST3Presets',
+                       'com.steinberg.ReWire',
+                       'com.steinberg.Retrologue',
+                       'com.steinberg.Retrologue2VST3Presets',
+                       'com.steinberg.RetrologueSubPresets',
+                       'com.steinberg.RetrologueVST3Presets',
+                       'com.steinberg.RockPopToolboxMIDIFiles',
+                       'com.steinberg.RockPopToolboxPatternBanks',
+                       'com.steinberg.UploadManager',
+                       'com.steinberg.euconappsets',
+                       'com.steinberg.euconvstxmls',
+                       'com.steinberg.nuagevstxmls',
+                       'com.steinberg.padshop.documentation',
+                       'com.steinberg.vst3vsttransitextension',
+                       'com.steinberg.vsttransitcore',
+                       # The following relate to Retrologue & Padshop installers
+                       'com.steinberg.Documentation',
+                       'com.steinberg.PadshopAU',
+                       'com.steinberg.PadshopAU.padshop',
+                       'com.steinberg.RetrologueAU.retrologue',
+                     ]
+
+  zap delete: [
+                '~/Library/Caches/com.steinberg.cubase10v5',
+                '~/Library/Preferences/Application Files/Cubase 10.5',
+                '~/Library/Preferences/com.steinberg.cubase10v5.plist',
+                '~/Library/Preferences/Cubase 10.5',
+              ]
+end
